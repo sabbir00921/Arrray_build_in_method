37d63f9be74e50fba33d6b23270e0a46ec4d2827
sabbir00921 <sabbir00921@gmail.com>
2024-12-17 14:43:19 +0600

    Array method practice and also describe

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//.join()  * replace with comma in a array.
+const myArr = ["sabbir", "hossain", "gazipur", "Diu"]
+let newArray = myArr.join("_")
+console.log(newArray) // Output: sabbir_hossain_gazipur_Diu
+
+//.pop() & .push() work in left side
+// .pop()  * Delete last item in a array 
+//const popArr = myArr.pop()
+console.log(`Deleted last items is: ${myArr.pop()}`) //myArr.pop() * shows only deleted items & directely mutant main array
+console.log(myArr)
+
+// .push()  * Addeed last item in a array 
+myArr.push("mash", "heart") // Directly store in main array
+console.log(myArr)
+
+//shift() & .unshift() work in left side
+// .shift()  * Delete frist item in a array 
+console.log(`Deleted last items is: ${myArr.shift()}`) //myArr.push() * shows only deleted items & directely mutant main array
+console.log(myArr)
+
+// .unshift()  * Addeed frist item in a array 
+myArr.unshift("virus", "guru") // Directly store in main array from left
+console.log(myArr)
