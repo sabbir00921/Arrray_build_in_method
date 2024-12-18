792fd2130d01aab6a8090b6ff4bf058e2dab7c6b
sabbir00921 <sabbir00921@gmail.com>
2024-12-18 20:07:09 +0600

    min_max & sort practice

---
+++ Max_min/main.js
@@ -0,0 +1,7 @@
+const arr= [51,8,1,5,6,8,4,455,66,4,54,78,52]
+const min = Math.min.apply(null, arr)
+console.log(min);
+
+const max = Math.max.apply(null, arr)
+console.log(max);
+
---
+++ Object_sort/js/main.js
@@ -0,0 +1,17 @@
+const info = [
+    {
+        name: "sabbir", age: 28
+    },
+    {
+        name: "mash", age: 23
+    },
+    {
+        name: "rakib", age: 26
+    },
+    {
+        name: "raju", age: 29
+    },
+]
+
+const sortedInfo = info.sort(info.name)
+console.log(sortedInfo);
