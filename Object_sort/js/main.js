f37fa90dd25a41a9e43957720faee2ae9b12a2fe
sabbir00921 <sabbir00921@gmail.com>
2024-12-19 16:54:21 +0600

    Sort an object element by using neumeric valu

--- Object_sort/js/main.js
+++ Object_sort/js/main.js
@@ -11,7 +11,17 @@ const info = [
     {
         name: "raju", age: 29
     },
+    {
+        name: "alex", age: 15
+    },
+    {
+        name: "chotu", age: 20
+    },
+    {
+        name: "zack", age: 31
+    }
 ]
 
-const sortedInfo = info.sort(info.name)
+// const sortedInfo = info.sort(info.name) //
+const sortedInfo = info.sort((a,b)=> a.age -b.age)
 console.log(sortedInfo);
